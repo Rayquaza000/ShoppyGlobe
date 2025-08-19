dac4ed3dcbe2437877b193880051425f2bbfa0df
Rayquaza000 <mehtapratik2001@gmail.com>
2025-08-19 13:08:55 +0530

    final

--- src/component/SearchPage.jsx
+++ src/component/SearchPage.jsx
@@ -12,7 +12,7 @@ function SearchPage() {
     <>
     <span className='mt-5 text-[24px] mx-10'>Search results for {searchValue}:</span>
     <div className='flex flex-wrap'>
-        {filteredData.length>0?
+        {filteredData?.length>0?
             filteredData?.map((item,index)=>{
                 return <ProductCard key={item.id} id={item.id} loading={loading} imgsrc={item.thumbnail} brand={item.brand} title={item.title} avgRating={item.rating} warrantyInfo={item.warrantyInformation} availabilityStatus={item.availabilityStatus} shippingInfo={item.shippingInformation} price={item.price}/>
             }):<div className='font-[20px] mt-3 mx-10'>No results found</div>
